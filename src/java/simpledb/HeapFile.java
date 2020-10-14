f4d4ccc30d093a12afbb1af6672b6984b49024ce
woshihuixiong <372397266@qq.com>
2020-10-14 18:27:39 +0800

    Lab1_Exercise5

--- src/java/simpledb/HeapFile.java
+++ src/java/simpledb/HeapFile.java
@@ -22,8 +22,12 @@ public class HeapFile implements DbFile {
      *            the file that stores the on-disk backing store for this heap
      *            file.
      */
+    private File file;
+    private TupleDesc tupleDesc;
     public HeapFile(File f, TupleDesc td) {
         // some code goes here
+        this.file = f;
+        this.tupleDesc = td;
     }
 
     /**
@@ -33,7 +37,7 @@ public class HeapFile implements DbFile {
      */
     public File getFile() {
         // some code goes here
-        return null;
+        return file.getAbsoluteFile();
     }
 
     /**
@@ -47,7 +51,8 @@ public class HeapFile implements DbFile {
      */
     public int getId() {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        // throw new UnsupportedOperationException("implement this");
+        return file.getAbsoluteFile().hashCode();
     }
 
     /**
@@ -57,13 +62,39 @@ public class HeapFile implements DbFile {
      */
     public TupleDesc getTupleDesc() {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        // throw new UnsupportedOperationException("implement this");
+        return tupleDesc;
     }
 
     // see DbFile.java for javadocs
+    // BufferPool如果要读取新page就会调用这个方法
     public Page readPage(PageId pid) {
         // some code goes here
-        return null;
+        FileInputStream fileInputStream = null;
+        HeapPage heapPage = null;
+        try{
+            //文件输入流，指向file的第一个字节，不是直接把整个文件先读进内存
+            fileInputStream = new FileInputStream(file);
+            byte[] buff = new byte[BufferPool.getPageSize()];
+            //这里调用缓冲池方法只是获取页面大小，与读取页面无直接关联！
+            //要从一个文件读取第pid个页面，就要跳过前面的页面，比如读取pid=3页面就要跳过(0,1,2)前面3个页面*页面大小个字节数（调用的是静态方法）
+            fileInputStream.skip(pid.getPageNumber()*BufferPool.getPageSize());
+            if(fileInputStream.read(buff) != -1){
+                heapPage = new HeapPage(new HeapPageId(pid.getTableId(), pid.getPageNumber()), buff);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally {
+            if(fileInputStream != null){
+                try {
+                    fileInputStream.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                    System.out.println("Cannot close fileInputStream!");
+                }
+            }
+        }
+        return heapPage;
     }
 
     // see DbFile.java for javadocs
@@ -77,7 +108,8 @@ public class HeapFile implements DbFile {
      */
     public int numPages() {
         // some code goes here
-        return 0;
+        if(file.length()%BufferPool.getPageSize() == 0) return (int)(file.length()/BufferPool.getPageSize());
+        return (int)file.length()/BufferPool.getPageSize()+1;
     }
 
     // see DbFile.java for javadocs
@@ -97,9 +129,63 @@ public class HeapFile implements DbFile {
     }
 
     // see DbFile.java for javadocs
+    //遍历HeapFile中的每个tuple，必须使用BufferPool.getPage()访问HeapFile中的页。此方法将页面加载到缓冲池当中。
     public DbFileIterator iterator(TransactionId tid) {
         // some code goes here
-        return null;
+        return new DbFileIterator() {
+            int numPage = numPages();
+            int cur = 0;
+            Iterator<Tuple> tupleIterator;
+            boolean isOpen = false;
+            @Override
+            public void open() throws DbException, TransactionAbortedException {
+                isOpen = true;
+                cur = 0;
+                //一开始指向null
+                tupleIterator = null;
+            }
+
+            @Override
+            public boolean hasNext() throws DbException, TransactionAbortedException {
+                if(!isOpen) return false;
+                //如果是一开始，获得第一个页面的迭代器
+                if(tupleIterator == null){
+                    HeapPage heapPage = (HeapPage)Database.getBufferPool().getPage(tid,new HeapPageId(getId(),cur),Permissions.READ_ONLY);
+                    tupleIterator = heapPage.iterator();
+                    cur++;
+                }
+                //如果当前这个页面的迭代器有下一个tuple则返回true
+                if(tupleIterator.hasNext()) return true;
+                //如果没有，则寻找下一个hasNext=true的页面的迭代器
+                while(cur<numPage){
+                    HeapPage heapPage = (HeapPage)Database.getBufferPool().getPage(tid,new HeapPageId(getId(),cur),Permissions.READ_ONLY);
+                    tupleIterator = heapPage.iterator();
+                    cur++;
+                    if(tupleIterator.hasNext()) return true;
+                }
+                //找到最后一个页面都没找到，返回false
+                return false;
+            }
+
+            @Override
+            public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+                if(hasNext()){
+                    return tupleIterator.next();
+                }
+                throw new NoSuchElementException();
+            }
+
+            @Override
+            public void rewind() throws DbException, TransactionAbortedException {
+                close();
+                open();
+            }
+
+            @Override
+            public void close() {
+                isOpen = false;
+            }
+        };
     }
 
 }
